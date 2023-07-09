98e7def625166c1d19f09bd94c3e68ef87973a52
Mastan <121248741+MastanOfficials@users.noreply.github.com>
2023-07-09 21:33:34 +0530

    Revert "Draft/youthful grothendieck"

--- react-redux/src/store.js
+++
@@ -1,9 +0,0 @@
-import {legacy_createStore as createStore,applyMiddleware, combineReducers} from "redux";
-import thunk from "redux-thunk";
-import { reducers } from "./redux/reducers";
-
-const rootReducer=combineReducers({
-    data:reducers
-})
-
-export const store=createStore(rootReducer,applyMiddleware(thunk));
